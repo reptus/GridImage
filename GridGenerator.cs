8e5d02ed550821dc7032b1aa300a72fcbd4d031f
“Rafael <reptus@hotmail.com>
2023-11-24 18:01:31 -0500

    Add project files.

---
+++ GridGenerator.cs
@@ -0,0 +1,79 @@
+﻿using System.Drawing;
+using System.Drawing.Imaging;
+
+namespace GridImage
+{
+    internal class GridGenerator(string path, int rows, int cols, int thickness)
+    {
+        Bitmap? _image;
+
+        public void GenerateGrid()
+        {
+            _image = OpenImage(path);
+            if (_image != null)
+            {
+                for (int i = 1; i < rows; i++)
+                {
+                    CreateHorizontalLine(i);
+                }
+                for (int i = 1; i < cols; i++)
+                {
+                    CreateVerticalLine(i);
+                }
+                SaveFile();
+            }
+        }
+
+        private void CreateVerticalLine(int col)
+        {
+            int posX = col * _image.Width / cols;
+            for (int j = 0; j < _image.Height; j++)
+            {
+                for (int k = 0; k < thickness; k++)
+                {
+                    _image.SetPixel(posX + k, j, Color.OrangeRed);
+                }
+            }
+        }
+
+        private void CreateHorizontalLine(int row)
+        {
+            int posY = row * _image.Height / rows;
+            for (int j = 0; j < _image.Width; j++)
+            {
+                for (int k = 0; k < thickness; k++)
+                {
+                    _image.SetPixel(j, posY + k, Color.OrangeRed);
+                }
+            }
+        }
+
+        private static Bitmap? OpenImage(string path)
+        {
+            try
+            {
+                return new(Image.FromFile(path));
+            }
+            catch
+            {
+                Console.WriteLine("ERROR: Can not open file. Check path and file extension.");
+                return null;
+            }
+        }
+
+        private void SaveFile()
+        {
+            var newFile = GetNewFileName();
+            _image.Save(newFile, ImageFormat.Png);
+            Console.WriteLine("Grid image created with name {0}", newFile);
+        }
+
+        private string GetNewFileName()
+        {
+            var imageName = Path.GetFileName(path);
+            imageName = imageName.Substring(0, imageName.IndexOf("."));
+            imageName += "_grid.png";
+            return Path.Combine(Path.GetDirectoryName(path), imageName);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Drawing;
+
+namespace GridImage
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            int rows = 3, cols = 3, thickness = 1;
+            if (args.Length >= 3)
+            {
+                if (!int.TryParse(args[1], out rows) || rows < 0)
+                {
+                    throw new Exception("Argument 2 (number of rows) must be a positive integer");
+                }
+                if (!int.TryParse(args[2], out cols) || cols < 0)
+                {
+                    throw new Exception("Argument 3 (number of columns) must be a positive integer");
+                }
+                if (args.Length == 4 && (!int.TryParse(args[3], out thickness) || thickness <= 0))
+                {
+                    throw new Exception("Argument 4 (thickness) must be a positive integer greater than zero");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Usage: GridGenerator.exe filename rows columns [thickness]");
+                Console.WriteLine();
+                Console.WriteLine("Generating grid with default values");
+                Console.WriteLine();
+            }
+
+            var gg = new GridGenerator(args[0], rows, cols, thickness);
+            gg.GenerateGrid();
+            
+        }
+
+        
+    }
+}
